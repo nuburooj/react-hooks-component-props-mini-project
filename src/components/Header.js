02b0e7f27a03a2bcbac7841ddf3d9338460d80d1
Nuburooj Khattak <nuburoojkhattak@gmail.com>
2024-01-15 23:35:41 -0500

    complete lab

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header({name}){
+    return(
+        <header>
+            <h1>{name}</h1>
+        </header>
+    )
+}
+
+
+
+
+
+
+
+export default Header;
